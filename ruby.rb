b49e68c9987f74d9605de093525c5ae7d421f8d2
λx . ophidia ereц grex⋆. 𐙚 ̊      .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-04-15 05:33:19 -0700

    Update ruby.rb
    # grep SNDCTL_MIDI_SETMODE /usr/include/sys/soundcard.h

--- ruby.rb
+++ ruby.rb
@@ -1,1 +1,14 @@
+# grep SNDCTL_MIDI_SETMODE /usr/include/sys/soundcard.h
+
+require 'ffi'
+
+
+module Soundcard
+  extend FFI::Library
+  ffi_lib FFI::Library::LIBC
+
+  SNDCTL_MIDI_SETMODE = 0x40044d01 # must match your system header
+end
+
+
 
