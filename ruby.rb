f38455b8b7ef2e6f4226819e62282b897352c09b
λx . ophidia ereц grex⋆. 𐙚 ̊      .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-04-15 05:36:44 -0700

    Add FFI bindings for MIDI ioctl operations

--- ruby.rb
+++ ruby.rb
@@ -2,7 +2,7 @@
 
 require 'ffi'
 
-
+#
 module Soundcard
   extend FFI::Library
   ffi_lib FFI::Library::LIBC
@@ -10,5 +10,35 @@ module Soundcard
   SNDCTL_MIDI_SETMODE = 0x40044d01 # must match your system header
 end
 
+#
+int mode = MIDI_MODE_TRADITIONAL; // or TIMED / ALTERNATIVE
+ioctl(fd, SNDCTL_MIDI_SETMODE, &mode);
 
+# 
+module LibC
+  extend FFI::Library
+  ffi_lib FFI::Library::LIBC
 
+  attach_function :ioctl, [:int, :ulong, :pointer], :int
+end
+
+#
+# Constants (values must match your system headers)
+SNDCTL_MIDI_SETMODE   = 0x40044d01   # verify this!
+MIDI_MODE_TRADITIONAL = 0
+MIDI_MODE_TIMED       = 1
+MIDI_MODE_ALTERNATIVE = 2
+
+#
+fd = IO.sysopen("/dev/midi", "r+")
+
+# Allocate an int and set value
+mode_ptr = FFI::MemoryPointer.new(:int)
+mode_ptr.write_int(MIDI_MODE_TRADITIONAL)
+
+# Call ioctl
+ret = LibC.ioctl(fd, SNDCTL_MIDI_SETMODE, mode_ptr)
+
+if ret == -1
+  raise SystemCallError.new("ioctl failed", FFI.errno)
+end
